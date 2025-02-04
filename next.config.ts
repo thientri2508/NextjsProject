9084bf55c1f3103e57e0425a0682074a95d3f62b
thientri2508 <ynhi572@gmail.com>
2025-02-04 16:16:16 +0700

    tri

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: isProd ? "/NextjsProject" : "",  // 🛠 Thay bằng tên repo
+  assetPrefix: isProd ? "/NextjsProject/" : "", // Đảm bảo đường dẫn asset đúng
+  images: {
+    unoptimized: true, // GitHub Pages không hỗ trợ tối ưu ảnh Next.js
+  },
 };
 
 export default nextConfig;
