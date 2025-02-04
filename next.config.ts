e76c7fb55a6613cdd34160633762e740d905245a
thientri2508 <ynhi572@gmail.com>
2025-02-04 14:58:29 +0700

    aa

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
