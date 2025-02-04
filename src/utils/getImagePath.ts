69e38ddba9d6746246f0d9c4fe653bdaf6973e8b
thientri2508 <ynhi572@gmail.com>
2025-02-04 16:55:58 +0700

    asdf

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -1,8 +1,7 @@
 import getConfig from "next/config";
 
 export function getImagePath(imagePath: string) {
-    const config = getConfig();
-    const basePath = config?.publicRuntimeConfig?.basePath || "";
-  
-    return `${basePath}${imagePath}`;
-  }
+  const { publicRuntimeConfig } = getConfig();
+  const basePath = publicRuntimeConfig?.basePath || ""; // Lấy basePath từ next.config.js
+  return `${basePath}${imagePath}`;
+}
