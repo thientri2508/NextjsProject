85db544a6ba6306900777752a0c4f1f9362db121
thientri2508 <ynhi572@gmail.com>
2025-02-04 17:12:24 +0700

    gsdfg

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   basePath: isProd ? "/NextjsProject" : "",
   assetPrefix: isProd ? "/NextjsProject/" : "",
   images: {
